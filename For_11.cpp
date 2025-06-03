7024fd3b1687f7e96f4801587866f6ad52e58921
00136225 <00136225@uca.edu.sv>
2025-06-03 10:38:50 -0600

    nuevos

--- For_11.cpp
+++ For_11.cpp
@@ -5,7 +5,19 @@
 using namespace std;
 
 int main() {
+ int N, suma = 0;
 
+    //ingresar un numero
+    cout << "Ingresa cualquier numero: ";
+    cin >> N;
+
+    // Ciclo for para sumar los números desde 1 hasta N
+    for (int i = 1; i <= N; i++) {
+        suma += i;  // Sumar el valor de i a la variable suma
+    }
+
+    // Imprimir el resultado de la suma
+    cout << "La suma de los primeros " << N << " números es: " << suma << endl;
 
     return 0;
 }
--- IF_ELSE19.cpp
+++ IF_ELSE19.cpp
@@ -1,12 +1,43 @@
 
-//Tarifa de Luz Eléctrica
-//Calcula la tarifa según el consumo: menor a 100 kWh, tarifa baja; entre 100 y 200 kWh, tarifa media; más de 200 kWh, tarifa alta.
+// Tarifa de Luz Eléctrica
+// Calcula la tarifa según el consumo: menor a 100 kWh, tarifa baja; entre 100 y 200 kWh, tarifa media; más de 200 kWh, tarifa alta.
 
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main() {
+int main()
+{
 
+    int consumo;
+    string Tarifa;
+
+    cout << " por favor digite su tarifa  " << endl;
+    cin >> consumo;
+
+    if (consumo <= 100)
+
+    {
+        Tarifa = 0.192255;
+        cout << " su tarifa es baja" << endl;
+    }
+    else if (consumo >= 100 && consumo <= 200)
+    {
+        Tarifa = 0.192266;
+        cout << " su tarifa es media " << endl;
+    }
+    else if (consumo >= 200)
+    {
+        Tarifa = 0.190757;
+        cout << "Su tarifa es alta" << endl;
+    }
+    else
+    {
+        cout << " Invalido vuelva a digitar su tarifa " << endl;
+    }
+
+    cout << " su consumo es  de; " << consumo << "Khw; " << endl;
+    cout << " La tarifa por consumo es de; " << Tarifa << endl;
 
     return 0;
 }
